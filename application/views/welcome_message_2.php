14309a054d37b391efdb76807fcbc52fe6331ae1
Jasver Anlouise <jassysalva@gmail.com>
2017-01-29 13:19:54 +0800

    combined

--- application/views/welcome_message_2.php
+++ application/views/welcome_message_2.php
@@ -62,6 +62,7 @@ hr{
 .postMargin{
 	margin-bottom: 10px;
 	margin-top: -5px;
+	margin-right: 10px;
 }
 .postModal{
 	background-color: darkred;
@@ -81,6 +82,33 @@ hr{
 .infoMargin{
 	margin: 0px 5px 5px 20px
 }
+.imageCircle{
+	border: solid;
+	border-width: 1.9px;
+	border-color: black;
+}
+.pointer{
+	cursor: pointer;
+}
+.inputWidth{
+	width: 90%;
+	padding-left: 5px;
+}
+.paddingtb{
+	padding-top: 2px;
+	padding-bottom: 2px;
+}
+.marginTop{
+	margin-top: -5px;
+}
+.commentBox{
+	width: 90%;
+	padding: 3px;
+}
+.borderMargin{
+	margin-top: 25px;
+	margin-bottom: 25px;
+}
 </style>	
 	
 <body style = "background-color: #fbf3f2;">
@@ -91,7 +119,7 @@ hr{
   class="w3-closenav w3-large " style = "padding: 3%"  > <font color = "white">  Close &times; </font></a>
   <a style = "padding: 4%;" href="tryit_426.htm#"><font color = "white" size = "3" style = "margin-left: 25%;">
   <img style = "margin-right: 6%;" src="<?php echo base_url();?>img/home_2.png" alt="Smiley face" height="20" width="20">Feed</font></a>
-  <a style = "padding: 4%" href="tryit_426.htm#"><font color = "white" size = "3" style = "margin-left: 25%;" >
+  <a style = "padding: 4%" href="profile"><font color = "white" size = "3" style = "margin-left: 25%;" >
   <img style = "margin-right: 6%" src="<?php echo base_url();?>img/profile_2.png" alt="Smiley face" height="20" width="20">Profile</font></a>
   <a style = "padding: 4%" href="tryit_426.htm#"><font color = "white" size = "3" style = "margin-left: 25%;" >
   <img style = "margin-right: 6%" src="<?php echo base_url();?>img/people_2.png" alt="Smiley face" height="20" width="20">People</font></a>
@@ -106,12 +134,12 @@ hr{
 <header style = "padding: 1%" class="w3-border-bottom w3-border-black theme w3-container fix width">
     
 	<span class="w3-opennav w3-xlarge" onclick="w3_open()" id="openNav">&#9776;</span> 
-	<font face = "SqueakyChalkSound" size = "5" color = "white"> &nbsp; 	U share </font>
+	<font face = "Eraser" size = "5" color = "white"> &nbsp; 	U share </font>
 	<font color = "white" size = "6"> | </font>	  <font face = "Century Gothic" size = "5" color = "white"> Confessions </font>
     <div style = "float: right; margin-right: 2%; ">
 		<font face = "Calibri" size = "6" color = "white">  Jasver Salva </font>
 		<a onclick="document.getElementById('id02').style.display='block'">
-			<img style = "margin: 0% 0% -5% 0%" src="<?php echo base_url();?>img/user1.png" alt="Smiley face" height="40" width="40"> 
+			<img class = "pointer" style = "margin: 0% 0% -5% 0%" src="<?php echo base_url();?>img/user1.png" alt="Smiley face" height="40" width="40"> 
 		</a>
 	</div>
 </header>
@@ -128,16 +156,25 @@ hr{
 		<?php foreach($posts as $post){?>
 		
 		<img style = "margin: 0% 0% -1.5% 0%" src="<?php echo base_url();?>img/user1.png" alt="Smiley face" height="30" width="30">
-		<font face = "Calibri" size = "4" color = "black"> &nbsp; <?php echo $post->hidden_name;?>| </font> <font face = "Calibri" size = "4" color = "darkgray"> <?php echo $post->college;?>	 </font>
+		<font face = "Calibri" size = "4" color = "black"> &nbsp; <?php echo $post->hidden_name;?> | </font> <font face = "Calibri" size = "4" color = "darkgray"> <?php echo $post->college;?>	 </font>
 		<div style = "margin-top: -1.5%" class="w3-container w3-section w3-pale-red w3-round w3-border">
-			<p><font color = "darkred"> <b><?php echo $post->confession_title;?></b></font></p>
-			<p><?php echo $post->confession_text;?>
-			<hr></p>
+			<p>
+				<font face = "Calibri" size = "4" color = "darkred"> 
+					<b><?php echo $post->confession_title;?></b>
+				</font>
+			</p>		
+			
+		<p class = "marginTop" ><pre><font face = "Calibri" size = "4" ><?php echo $post->confession_text;?></font></pre><hr>
+			</p> 
+			
+			
 			
 			<div> 
-				<a href = "#"> <img class = "postMargin left" src="<?php echo base_url();?>img/agree.png" alt="Smiley face" height="20" width="20"> <font class = "postMargin left" style = "margin-right: 10px;" size = "2" >Agree </font></a> 
-				<a href = "#"> <img style = "margin: 0% 0% -5% 0%" class = "postMargin left" src="<?php echo base_url();?>img/disagree.png" alt="Smiley face" height="20" width="20"> <font class = "postMargin left" style = "margin-left: 20px;"size = "2" >Disagree </font> </a>
-				<font class = "postMargin right" size = "2" ><?php echo $post->time;?>/<?php echo $post->date;?> </font>
+				<a href = "#"> <img style = "margin: -1.2% 0% -5% 0%" class = "postMargin left" src="<?php echo base_url();?>img/agree.png" alt="Smiley face" height="20" width="20"> </a>
+				<a href = "#"> <img style = "margin: -.5% 0% -5% 5%" class = "postMargin left" src="<?php echo base_url();?>img/disagree.png" alt="Smiley face" height="20" width="20">  </a>
+				<a class = "pointer" onclick="document.getElementById('id04').style.display='block'"> <font class = "postMargin left"  style = "margin-left: 65px;" size = "2" >Comments </font> </a>
+				
+				<font class = "postMargin right" size = "2" ><?php echo $post->time;?> / <?php echo $post->date;?> </font>
 			</div>
 		</div>
 		
@@ -175,11 +212,11 @@ function w3_close() {
 <a  onclick="document.getElementById('id01').style.display='block'" class="w3-btn-floating-large w3-pale-red postButton"><font color = "red">+</font></a> 
 
 <div id="id01" class="w3-modal">
-  <div style = "width: 40%; margin-top: -3%;" class="w3-modal-content w3-animate-top">
+  <div style = "width: 40%; margin-top: -3%; margin-bottom: 3%;" class="w3-modal-content w3-animate-top">
     <header class="w3-container postModal">
       <span onclick="document.getElementById('id01').style.display='none'"
       class="w3-closebtn">&times;</span>
-      <h4>Confess</h4>
+      <h4><img style = "margin: -15% -.9% -2% -1%" src="<?php echo base_url();?>img/confession.png" height="35" width="35"> onfess</h4>
     </header>
     <div class="w3-container ">
 	
@@ -205,7 +242,7 @@ function w3_close() {
 				<font style = "Century Gothic" size = "2" color = "red"> <br> *You will be notified once confession request is approved. </font>
 				<input style = "margin: -4px 0px 10px 0px;" class = "padding right w3-btn w3-white w3-border w3-border-red" type = "submit" value = "Confess" >
 			</form>
-			
+			<br>
 			
 		</p>
     </div>
@@ -213,45 +250,422 @@ function w3_close() {
 </div>
 
 <div id="id02" class="w3-modal">
-  <div style = "width: 40%; margin-top: -3%;" class="w3-modal-content w3-animate-top">
+ 
+  <div style = "width: 40%; margin-top: -1%; margin-bottom: 3%;" class="w3-modal-content w3-animate-top">
     <header class="w3-container postModal">
-      <span onclick="document.getElementById('id02').style.display='none'"
-      class="w3-closebtn">&times;</span>
-      <h4>Full Name</h4>
+		  <span onclick="document.getElementById('id02').style.display='none'"
+		  class="w3-closebtn">&times;</span>
+		  <center><img class = "w3-circle imageCircle" style = "margin: -15% 0% -1.5% -1%" src="<?php echo base_url();?>img/hakeem_2.jpg" alt="Smiley face" height="150" width="150"> </center>
+		  <h4><center>Hakeem Polistico</center></h4>
     </header>
     <div class="w3-container ">
-		<p> <b>Info</b> </p>
-		<p class = "infoMargin" > Email: </p>
-		<p class = "infoMargin"> Contact No: </p>
-		<p class = "infoMargin"> Birthday: </p>
-		<p class = "infoMargin"> Gender: </p>
+	
+	<font face = "Century Gothic" size = "3" >
+		
+		<p> <b style = "left">Info</b> <a class = "pointer" onclick="document.getElementById('id03').style.display='block'" style = "float: right;" > edit </a></p>
+		<table>
+			<tr >
+				<td width = "25%">
+					<p class = "infoMargin" > Email:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> hjpolistico@gmail.com </font></p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > Contact No:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> 0997-586-4782 </font></p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > Birthday:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> July 22 </font></p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > Sex:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> Male </font></p>
+				</td>
+			</tr>
+		</table>
 		<hr>
 		
 		<p> <b>Student Info</b> </p>
-		<p class = "infoMargin" > Student No: </p>
-		<p class = "infoMargin"> Course: </p>
-		<p class = "infoMargin"> College: </p>
+		<table>
+			<tr >
+				<td width = "25%">
+					<p class = "infoMargin" > Student No:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> 14-038-014 </font></p>
+				</td>
+			</tr>
+			 
+			<tr>
+				<td>
+					<p class = "infoMargin" > Course:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> Bachelor of Science in Information System </font></p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > College:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> College of Science </font></p>
+				</td>
+			</tr>
+			
+			<tr >
+				<td width = "25%">
+					<p class = "infoMargin" > Year Graduated:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> N/A </font></p>
+				</td>
+			</tr>
+			
+			
+			
+			
+			
+		</table>
 		<hr>
 		
 		<p> <b>Confessions</b> </p>
-		<p class = "infoMargin" > Confessions Approved: </p>
-		<p class = "infoMargin"> Confessed: </p>
+		<table>
+			<tr >
+				<td width = "25%">
+					<p class = "infoMargin" > Confession Approved:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> 2 </font></p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > Confessions:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> 10 </font></p>
+				</td>
+			</tr>
+						
+		</table>
 		
 		<br>
+	</font>
     </div>
+  </div>
+
+</div>
+
+<div id="id03" class="w3-modal">
+ 
+  <div style = "width: 40%; margin-top: -1%; margin-bottom: 3%;" class="w3-modal-content w3-animate-top">
+    <header class="w3-container postModal">
+		  <span onclick="document.getElementById('id03').style.display='none'"
+		  class="w3-closebtn">&times;</span>
+		  <center><img class = "w3-circle imageCircle" style = "margin: -15% 0% -1.5% -1%" src="<?php echo base_url();?>img/hakeem_2.jpg" alt="Smiley face" height="150" width="150"> </center>
+		  <h4><center>Hakeem Polistico</center></h4>
+    </header>
+    <div class="w3-container ">
+	
+	<font face = "Century Gothic" size = "3" >
+		
+		<p> <b>Info</b> </p>
+		<table>
+			<form>
+			<tr >
+				<td width = "25%">
+					<p class = "infoMargin" > Email:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" >  <input class = "inputWidth" type = "email" name = "email" > </p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > Contact No:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" ><input class = "inputWidth" type = "text" name = "contact_no" ></p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > Birthdate:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <input class = "inputWidth" type = "date" name = "birthdate" ></p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > Sex:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > 
+					<select class="w3-select w3-border paddingtb inputWidth" name="sex">
+						<option value="" disabled selected>Choose your option</option>
+						<option value="male">Male</option>
+						<option value="female">Female</option>
+					</select></p>
+				</td>
+			</tr>
+		</table>
+		<hr>
+		
+		<p> <b>Student Info</b> </p>
+		<table>
+			<tr >
+				<td width = "25%">
+					<p class = "infoMargin" > Student No:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <input class = "inputWidth" placeholder = "xx-xxx-xx" pattern = "\d{2}[\-]\d{3}[\-]\d{3}" type = "text" name = "student_no" ></p>
+				</td>
+			</tr>
+				
+			<tr>
+				<td>
+					<p class = "infoMargin" > Course:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <input class = "inputWidth" type = "text" name = "course" ></p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > College:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <select class="w3-select w3-border paddingtb inputWidth" name="college">
+						<option value="" disabled selected>Choose your option</option>
+						<option value="College of Science">College of Science</option>
+						<option value="College of Engineering">College of Engineering</option>
+						<option value="College of Industrial Technology">College of Industrial Technology</option>
+						<option value="College of Industrial Education">College of Industrial Education</option>
+						<option value="College of Architecture and Fine Arts">College of Architecture and Fine Arts</option>
+						<option value="College of Liberal Arts">College of Liberal Arts</option>
+					</select></p>
+				</td>
+			</tr>
+			
+			<tr >
+				<td width = "25%">
+					<p class = "infoMargin" > Year Graduated:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" ><input class = "inputWidth" type = "month" name = "year_graduated" ></p>
+				</td> 
+			</tr>
+			
+			
+			
+			
+			
+		</table>
+		<hr>
+		
+		<p> <b>Confessions</b> </p>
+		<table>
+			<tr >
+				<td width = "25%">
+					<p class = "infoMargin" > Confession Approved:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> 2 </font></p>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<p class = "infoMargin" > Confessions:</p>
+				</td>
+				
+				<td>
+					<p class = "infoMargin" > <font color = "gray"> 10 </font></p>
+				</td>
+			</tr>
+		</table>
+			<input style = "margin-left: 45%;" type = "submit" value = "save">
+		</form>			
+		
+		<br>
+		<br>
+	</font>
+    </div>
+  </div>
+
+</div>
+
+<div id="id04" class="w3-modal">
+  <div style = "width: 40%; margin-top: -3%; margin-bottom: 3%;" class="w3-modal-content w3-animate-top">
+    <header class="w3-container postModal">
+      <span onclick="document.getElementById('id04').style.display='none'"
+      class="w3-closebtn">&times;</span>
+      <h4><img style = "margin: -15% -.9% -2% -1%" src="<?php echo base_url();?>img/confession.png" height="35" width="35"> omments</h4>
+    </header>
+    <div class="w3-container ">
+	
+	<br>
+	
+		<img style = "margin: 0% 0% -1.5% 0%" src="<?php echo base_url();?>img/user1.png" alt="Smiley face" height="30" width="30">
+		<font faceimg style = "margin: 0% 0% -1.5% 0%" src="<?php echo base_url();?>img/user1.png" alt="Smiley face" height="30" width="30">		
+		<font face = "Calibri" size = "4" color = "black"> &nbsp; Hidden Name| </font> <font face = "Calibri" size = "4" color = "darkgray"> College </font>
+		<div style = "margin-top: -1.5%" class="w3-container w3-section w3-pale-red w3-round w3-border">
+			<p>
+				<font face = "Calibri" size = "4" color = "darkred"> 
+					<b>Confession Title</b>
+				</font>
+			</p>		
+			
+			<p class = "marginTop" ><font face = "Calibri" size = "4" >
+			Confession - Text, 
+			Confession - Text,
+			Confession - Text, 
+			Confession - Text, 
+			Confession - Text, </font><hr>
+			</p> 
+			
+			<div> 
+				<font class = "postMargin left" size = "2" >10 agrees / 2 disagrees </font>
+				<font class = "postMargin right" size = "2" >time / date </font>
+			</div>
+		</div>
+		
+		
+		<div class = "w3-border-bottom borderMargin" style = "margin-left: -16px; margin-right: -16px;" ></div>
+		
+		
+		<p>
+			
+			<form>
+				<img style = "margin: 0% 0% 0% 0%" src="<?php echo base_url();?>img/hakeem_2.jpg" class = "left w3-circle imageCircle" alt="Smiley face" height="35" width="35">
+				<input type = "text" style = "width: 90%;" class = "w3-input left" name = "comment" placeholder = "type your comment here">
+			</form>
+		</p>
+		<br><br>
+			<p>
+			<img style = "margin: 0% 0% -2.3% 0%" src="<?php echo base_url();?>img/hakeem_2.jpg" class = "w3-circle imageCircle" height="35" width="35">
+			
+				<font face = "Century Gothic" size = "3" color = "darkred"> 
+					
+					<b>Linkin Park</b>
+				</font>
+					
+				<font face = "Century Gothic" size = "3" color = "black">
+					I've become so numb, I can't feel you there. Become so tight so much more aware.
+				</font>
+				
+				<font face = "Century Gothic" size = "2" color = "darkgray">
+					time/date
+				</font>		
+		
+			</p>
+		<p>
+			<p>
+				<font face = "Century Gothic" size = "3" color = "darkred"> 
+					<img style = "margin: 0% 0% -2.3% 0%" src="<?php echo base_url();?>img/hakeem_2.jpg" class = "w3-circle imageCircle" height="35" width="35">
+					<b>Linkin Park</b>
+				</font>
+					
+				<font face = "Century Gothic" size = "3" color = "black">
+					In the end it doesn't really matter.
+				</font>
+				
+				<font face = "Century Gothic" size = "2" color = "darkgray">
+					time/date
+				</font>
+			</p>		
+		
+		</p>
+		
+		<p>
+			
+			<p>
+				<font face = "Century Gothic" size = "3" color = "darkred"> 
+					<img style = "margin: 0% 0% -2.3% 0%" src="<?php echo base_url();?>img/hakeem_2.jpg" class = "w3-circle imageCircle" height="35" width="35">
+					<b>Linkin Park</b>
+				</font>
+					
+				<font face = "Century Gothic" size = "3" color = "black">
+					We can't wait to burn it to the ground.
+				</font>
+				
+				<font face = "Century Gothic" size = "2" color = "darkgray">
+					time/date
+				</font>
+			</p>		
+		
+		</p>
+		
+		<br>
   </div>
 </div>
 
 <script>
 // Get the modal
 var modal = document.getElementById('id01');
-var modal = document.getElementById('id02');
+var modal2 = document.getElementById('id02');
+var modal3 = document.getElementById('id03');
+var modal4 = document.getElementById('id04');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+	if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+	if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+	if (event.target == modal4) {
+        modal4.style.display = "none";
+    }
+	
 }
 </script>
 
